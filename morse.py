5c89516bed43f16cd9f2f745af5cf6d9ef94cd66
czyzewski03 <czyzewski03@gmail.com>
2019-08-01 10:15:27 +0100

    Added 'encode' function

--- morse.py
+++ morse.py
@@ -1,3 +1,9 @@
+def encode(in_message):
+    """Converts a message to Morse code."""
+    out_message = [MORSE_CODE_DICTIONARY[char] for char in in_message.upper()]
+    return ' '.join(out_message)
+
+
 MORSE_CODE_DICTIONARY = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
     'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
